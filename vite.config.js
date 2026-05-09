3712ecb16998fbb1e4f3022e0859fe55e916dbd9
trungkien <kienn7454@gmail.com>
2026-05-09 23:05:32 +0700

    chore: configure Vite dev server and add Vercel deployment rewrites

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 4200,
-  },
+    plugins: [react()],
+    server: {
+        host: "0.0.0.0",
+        port: 4200,
+        strictPort: true
+    }
 })
